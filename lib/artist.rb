c9f36cf1f0db62f9790e5a40057812a500d8f08c
Sean Good <tree662@protonmail.com>
2019-12-04 20:26:59 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,10 @@
 require 'pry'
-require_relative './config.memorable.rb'
 
 class Artist
   extend Memorable 
+  
+  extend Memorable::ClassMethods
+  
   
   attr_accessor :name
   attr_reader :songs
