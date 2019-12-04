9e982b2525ae149688341009db734f7b247ce4b6
Sean Good <tree662@protonmail.com>
2019-12-04 20:24:02 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,18 +1,9 @@
 module Memorable
+  
   module InstanceMethods
     
   end
   
-  module ClassMethods
-    
-    def reset_all
-      all.clear
-    end
   
-    def count
-      all.count
-    end
-    
-  end
   
 end
