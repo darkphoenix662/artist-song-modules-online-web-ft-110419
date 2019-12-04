a5d5b615de48bceed8274efe8be5f33efd55a8a4
Sean Good <tree662@protonmail.com>
2019-12-04 20:16:02 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -5,7 +5,13 @@ module Memorable
   
   module ClassMethods
     
-    
+    def self.reset_all
+      self.all.clear
+    end
+  
+    def self.count
+      self.all.count
+    end
     
   end
   
